b8c0495fba23ad233308f82970f7db78f701ebe3
naseribrahim7072 <naseribrahim309@gmail.com>
2026-08-05 22:32:58 +0400

    Add files via upload

---
+++ file/program2.py
@@ -0,0 +1,13 @@
+grade=float(input("enter your grade please "))
+if grade>100:
+    print(" not possible")
+elif grade>=90:
+    print("your grade is A")
+elif grade>=80:
+    print("your grade is B")
+elif grade>=70:
+    print("your grade is C")
+elif grade>=60:
+    print("your grade is D")
+else:
+    print("your grade is F")
